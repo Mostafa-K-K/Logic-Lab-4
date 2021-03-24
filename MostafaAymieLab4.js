5abdc4f54b28cf2ddf52d6b11447f36bc0a682fb
Mostafa Kassem <moustafakassem12@gmail.com>
2021-03-24 18:36:46 +0200

    update

--- MostafaAymieLab4.js
+++ MostafaAymieLab4.js
@@ -1,3 +1,4 @@
+// //first way
 function getAvg() {
     const arr = [1, 2, 3];
     var sum = 0;
@@ -11,8 +12,21 @@ function getAvg() {
 }
 console.log(getAvg());
 
+// //second way
+// const getAvg = (...a) =>{
+//     var sum = 0;
+//     for(var i=0 ;i < a.length; i++){
+//         sum = sum + a[i];
+//     }
+//     div = sum / a.length;
+//     console.log(div);
+// }
+// getAvg(1, 2, 3, 4);
 
 
+
+
+// //zero way  :)
 const search = (y, x) => {
     var result = false;
     for (var i = 0; i < y.length; i++) {
@@ -24,8 +38,33 @@ const search = (y, x) => {
 }
 search([0, 1, 2, 3, 4], 8);
 
+// //first way
+// function search(arr, a) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (a === arr[i]) { return true; }
+//     } return false;
+// }
+
+// //second way
+// function search(arr, a) {
+//     const result = arr.filter(item => (item === a));
+//     return (result.length === 0) ? false : true;
+// }
+
+// //third way
+// var arr = [1, 2, 3, 4, 5];
+// const getAvg = a => {
+//     const avg = a.reduce((total, value) => { return total + value })
+//     const x = avg / a.length;
+//     return x;
+// }
+// console.log(getAvg(arr));
 
 
+
+
+
+// //first way
 const removeDuplicates = (x, y) => {
     for (var i = 0; i < x.length; i++) {
         var result = false;
@@ -41,7 +80,23 @@ const removeDuplicates = (x, y) => {
 }
 console.log(removeDuplicates([0, 1, 2, 3, 3, 5], []))
 
+// //second way
+// function  removeDuplicates(arr){
+//     var newArr = [];
+//      for (var i = 0; i < arr.length; i++) {
+//          if (newArr.indexOf(arr[i]) === -1) {
+//              newArr.push(arr[i]);
+//          }
+//      }
+//    return newArr;
 
+//  }
+//  console.log("new arr of [1,2,2,3,3,5] : " +removeDuplicates([1,2,2,3,3,5]));
+
+
+
+
+//first way
 function getMinMax() {
     var array = [3, 4, 5, 20, 25, 30];
     var max = array[0];
@@ -60,6 +115,21 @@ function getMinMax() {
 }
 console.log(getMinMax());
 
+// //second way
+// const getMinMax = (array, word) => {
+
+//     if (word == "min") {
+//         const min = array.reduce((min, item) => (min > item) ? item : min, array[0])
+//         return min
+//     } else {
+//         const max = array.reduce((max, item) => (max < item) ? item : max, array[0])
+//         return max
+//     }
+
+// }
+// console.log(getMinMax([10, 20, 30]))
+
+
 
 
 const shuffle = (x, y, z) => {
@@ -77,6 +147,20 @@ const shuffle = (x, y, z) => {
     return result;
 }
 console.log(shuffle([0, 1, 2, 3, 4, 5], [], []));
+
+// var arr = [0,1,2,3,4,5];
+// const shuffle = (a,b) =>{ 
+//     if(b == 'right'){
+//         const x= a.pop();
+//        a.unshift(x);
+//        console.log(a);
+//     }else if(b == 'left'){
+//         const y= a.shift();
+//         a.push(y);
+//         console.log(a);
+//      }
+// }
+// shuffle(arr, 'right');
 
 
 const intersect = (x, y, z) => {
